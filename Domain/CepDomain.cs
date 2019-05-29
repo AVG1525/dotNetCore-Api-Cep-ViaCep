60e1c185e302c02c8210dda16d20d295d6d77cde
Aron Vinicius <aronvinicius15@gmail.com>
2019-05-29 19:49:05 -0300

    Domains

---
+++ Domain/CepDomain.cs
@@ -0,0 +1,25 @@
+﻿using AspCepAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCepAPI.Domain
+{
+    public class CepDomain : DbContext
+    {
+        public CepDomain(DbContextOptions<CepDomain> options) : base(options)
+        {
+
+        } 
+
+        public DbSet<CepModel> CepModel { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<CepModel>().HasKey(m => m.cep);
+            base.OnModelCreating(builder);
+        }
+    } 
+}
