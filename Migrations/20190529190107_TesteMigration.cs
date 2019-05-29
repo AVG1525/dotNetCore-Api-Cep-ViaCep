78790a069f3348e9be27da2efbd35b97d95d450a
Aron Vinicius <aronvinicius15@gmail.com>
2019-05-29 19:51:26 -0300

    Migrations

---
+++ Migrations/20190529190107_TesteMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AspCepAPI.Migrations
+{
+    public partial class TesteMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CepModel",
+                columns: table => new
+                {
+                    cep = table.Column<string>(nullable: false),
+                    logradouro = table.Column<string>(nullable: true),
+                    complemento = table.Column<string>(nullable: true),
+                    bairro = table.Column<string>(nullable: true),
+                    localidade = table.Column<string>(nullable: true),
+                    uf = table.Column<string>(nullable: true),
+                    unidade = table.Column<string>(nullable: true),
+                    ibge = table.Column<string>(nullable: true),
+                    gia = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CepModel", x => x.cep);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CepModel");
+        }
+    }
+}
